e1fce85a4426693f7fa70cf6c6f497cfa72ff4e0
ilhamjws <143341327+ilhamjws@users.noreply.github.com>
2023-12-06 20:56:06 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,89 +1,45 @@
-import warnings
-warnings.filterwarnings('ignore')
-from tensorflow import keras
-from keras.layers import Input, Lambda, Dense, Flatten
-from keras.models import Model
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import preprocess_input
-from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
 import numpy as np
-from glob import glob
+import pandas as pd
 import matplotlib.pyplot as plt
 
-IMAGE_SIZE = [224, 224]
-
-train_path = 'C:/Users/ilham/Desktop/Datasets/chest_xray/train'
-valid_path = 'C:/Users/ilham/Desktop/Datasets/chest_xray/test'
-
-vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
-for layer in vgg.layers:
-    layer.trainable = False
-    folders = glob('C:/Users/ilham/Desktop/Datasets/chest_xray/train/*')
-x = Flatten()(vgg.output)
-prediction = Dense(len(folders), activation='softmax')(x)
-# create a model object
-model = Model(inputs=vgg.input, outputs=prediction)
-# view the structure of the model
-model.summary()
-model.compile(
-  loss='categorical_crossentropy',
-  optimizer='adam',
-  metrics=['accuracy']
-)
-from keras.preprocessing.image import ImageDataGenerator
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
-
-test_datagen = ImageDataGenerator(rescale = 1./255)
-
-
-
-
-# Make sure you provide the same target size as initialied for the image size
-training_set = train_datagen.flow_from_directory('C:/Users/ilham/Desktop/Datasets/chest_xray/train',
-                                                 target_size = (224, 224),
-                                                 batch_size = 10,
-                                                 class_mode = 'categorical')
-
-
-
-
-test_set = test_datagen.flow_from_directory('C:/Users/ilham/Desktop/Datasets/chest_xray/test',
-                                            target_size = (224, 224),
-                                            batch_size = 10,
-                                            class_mode = 'categorical')
-
-r = model.fit_generator(
-  training_set,
-  validation_data=test_set,
-  epochs=1,
-  steps_per_epoch=len(training_set),
-  validation_steps=len(test_set)
-)
 import tensorflow as tf
-from keras.models import load_model
+from tensorflow.keras import layers
 
-model.save('chest_xray.h5')
-from keras.models import load_model
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
-import numpy as np
-model=load_model('chest_xray.h5')
-img=image.load_img('C:\\Users\\ilham\\Desktop\\Datasets\\chest_xray\\val\\PNEUMONIA\\person1946_bacteria_4874.jpeg',target_size=(224,224))
-x=image.img_to_array(img)
-x=np.expand_dims(x, axis=0)
-img_data=preprocess_input(x)
-classes=model.predict(img_data)
-result=int(classes[0][0])
-if result>0.5:
-   print("Person is Affected By PNEUMONIA")
-else:
-   print("Result is Normal")
+train_dir = 'C:/Users/ilham/Desktop/Datasets/chest_xray/train'
+val_dir = 'C:/Users/ilham/Desktop/Datasets/chest_xray/val'
+test_dir = 'C:/Users/ilham/Desktop/Datasets/chest_xray/test'
 
+img_height = 128
+img_width = 128
+batch_size = 32
 
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    train_dir,
+    color_mode='grayscale',
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
 
-https://drive.google.com/drive/folders/13n-iylN3a2-eYR76nonw9F_S94HXQBF5?q=parent:13n-iylN3a2-eYR76nonw9F_S94HXQBF5 datasets (CHEST.XRAY.H5)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    val_dir,
+    color_mode='grayscale',
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    test_dir,
+    color_mode='grayscale',
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+
+train_ds.class_names
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        plt.subplot(3, 3, i + 1)
+        plt.imshow(np.squeeze(images[i].numpy().astype("uint8")))
+        plt.title(train_ds.class_names[labels[i]])
+        plt.axis("off")
+plt.show()
